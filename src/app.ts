a6f627abfc7bccc740427f660186be92513dcf40
Shinoy George <shinoy.george1987@gmail.com>
2026-02-01 11:03:22 -0500

    Day 3: config setup and basic logging middleware

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,22 @@
 import express, { Request, Response } from "express";
 import userRoutes from "./modules/users/user.routes";
-
+import {config} from "./config";
+import {logger} from "./middleware/logger";
 
 const app = express();
 
-app.use("/users", userRoutes)
+/*Middleware*/
+/*body parser*/
+app.use(express.json());
+app.use(logger);
 
+/*Routes*/
+app.use("/users", userRoutes)
 app.get("/health", (req: Request, res: Response) => {
     res.status(200).json({ status: "OK" });
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+
+app.listen(config.port, () => {
+    console.log(`Server running on port ${config.port}`);
 });
